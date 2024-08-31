0cc10be85b5dc5f3544a0a4fdff4593613fda4d7
Priyasanon <earningself919@gmail.com>
2024-09-01 00:58:23 +0530

    I commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const Register = require('./models/Register');
+const Register = require('./models/registerSchema');
 
 const app = express();
 const port = 3000;
